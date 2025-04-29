46fc5021e13e43cc5018bb24e403eb7c63d59e96
Ilya Kholodilov <kholodilov0601@gmail.com>
2025-04-29 22:07:01 +0300

    first commit

---
+++ src/test/java/gitTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class gitTest {
+
+        @Test
+        void testSearchGithub() {
+            //ОткрытьGithub
+            open("https://github.com");
+            //Меню -> Solutions -> Enterprize (с помощью команды hover для Solutions)
+            $(withText("Solutions")).hover();
+            $(withText("Enterprises")).click();
+            $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+        }
+}
+
+
+        @Test
+
